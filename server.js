062cf34b2bac5272ad73f9ac28c4fa0b2f4b2ff1
digitalservices-fhft <chris.topping1@nhs.net>
2025-07-02 20:10:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(cors());
 app.use(express.json());
 
