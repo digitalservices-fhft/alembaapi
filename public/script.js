f57b9dc13a14952000370b5e508d3d1fd63656b7
digitalservices-fhft <chris.topping1@nhs.net>
2025-07-03 14:19:49 +0100

    Remove cookies, added url parameter best practice

--- public/script.js
+++ public/script.js
@@ -1,62 +1,24 @@
 $(document).ready(function () {
-  // Helper: Get query parameter by name
-  function getQueryParam(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
-  }
+  // Use URLSearchParams best practice
+  const params = new URLSearchParams(window.location.search);
 
-  // Helper: Set a cookie
-  function setCookie(name, value, days) {
-    const d = new Date();
-    d.setTime(d.getTime() + (days*24*60*60*1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + encodeURIComponent(value) + ";" + expires + ";path=/";
-  }
-
-  // Helper: Get a cookie
-  function getCookie(name) {
-    let cname = name + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i].trim();
-      if (c.indexOf(cname) === 0) {
-        return c.substring(cname.length, c.length);
-      }
-    }
-    return "";
-  }
-
-  // Store parameters from URL into cookies if present
-  const paramsToStore = ['receivingGroup', 'customString1', 'configurationItemId', 'type', 'description'];
-  paramsToStore.forEach(function(param) {
-    const value = getQueryParam(param);
-    if (value !== null) {
-      setCookie(param, value, 1); // Store for 1 day
-    }
-  });
-
-  // Get value from URL, then cookie, then default
-  function getParamFromUrlOrCookie(param, defaultValue) {
-    const urlValue = getQueryParam(param);
-    if (urlValue !== null) return urlValue;
-    const cookieValue = getCookie(param);
-    if (cookieValue) return cookieValue;
-    return defaultValue;
+  // Helper: Get value from URL or default
+  function getParam(param, defaultValue) {
+    return params.has(param) ? params.get(param) : defaultValue;
   }
 
   // Set heading if title param exists
-  const boardTitle = getParamFromUrlOrCookie('title', null);
+  const boardTitle = getParam('title', null);
   if (boardTitle) {
     $('h1.mb-4').text(boardTitle);
   }
 
   // Get parameters for payload
-  const receivingGroup = getParamFromUrlOrCookie('receivingGroup', 13);
-  const customString1 = getParamFromUrlOrCookie('customString1', "Big Board ED Hub - Frimley");
-  const configurationItemId = getParamFromUrlOrCookie('configurationItemId', 5430);
-  const type = getParamFromUrlOrCookie('type', 143);
-  const description = getParamFromUrlOrCookie('description', "Ticket logged via API");
+  const receivingGroup = getParam('receivingGroup', 13);
+  const customString1 = getParam('customString1', "Big Board ED Hub - Frimley");
+  const configurationItemId = getParam('configurationItemId', 5430);
+  const type = getParam('type', 143);
+  const description = getParam('description', "Ticket logged via API");
 
   // Get token on page load
   let accessToken = '';
