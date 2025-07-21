386967ba6f53309df789ccf7625c39747f5c1fcb
Digital Services <chris.topping1@nhs.net>
2025-07-21 21:31:39 +0100

    ..

--- server.js
+++ server.js
@@ -337,7 +337,7 @@ await axios.post(
     });
     callReq.write(JSON.stringify(callPayload));
     callReq.end();
-  }
+  });
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
